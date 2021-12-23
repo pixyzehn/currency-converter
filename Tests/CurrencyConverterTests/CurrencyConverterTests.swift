1b9f9373273927dc94c81d3f41bf17b678379b80
Hiroki Nagasawa <2482478+pixyzehn@users.noreply.github.com>
2021-12-23 15:20:21 +0900

    Update tests

--- Tests/CurrencyConverterTests/CurrencyConverterTests.swift
+++ Tests/CurrencyConverterTests/CurrencyConverterTests.swift
@@ -2,74 +2,56 @@
 import XCTest
 
 final class CurrencyConverterTests: XCTestCase {
-    func testConverterFetch() {
+    func testConverterFetch() async throws {
         let converter = CurrencyConverter()
-        converter.fetch { result in
-            switch result {
-            case let .success(referenceRates):
-                // The reference rates are usually updated around 16:00 CET on every working day, except on TARGET closing days.
-                // Working hours: Monday to Friday: 8:30 to 17:30 CET. Saturday, Sunday, and public holidays: closed
-                // More info: https://www.ecb.europa.eu/home/contacts/working-hours/html/index.en.html
-                let date = referenceRates.date
-                // Given the Christmas holidays, the referenceRates should be updated within 4 days (3 [the Christmas holidays] + 1 [buffer for Timezone differences]).
-                // Otherwise, we see it as an exceptional error.
-                let now = Date()
-                let dateFormatter = DateFormatter.yyyyMMddFormatter
+        let referenceRates = try await converter.fetch()
+        // The reference rates are usually updated around 16:00 CET on every working day, except on TARGET closing days.
+        // Working hours: Monday to Friday: 8:30 to 17:30 CET. Saturday, Sunday, and public holidays: closed
+        // More info: https://www.ecb.europa.eu/home/contacts/working-hours/html/index.en.html
+        let date = referenceRates.date
+        // Given the Christmas holidays, the referenceRates should be updated within 4 days (3 [the Christmas holidays] + 1 [buffer for Timezone differences]).
+        // Otherwise, we see it as an exceptional error.
+        let now = Date()
+        let dateFormatter = DateFormatter.yyyyMMddFormatter
 
-                let today = dateFormatter.string(from: now)
-                let oneDayAgo = dateFormatter.string(from: now.addingTimeInterval(-24 * 60 * 60 * 1))
-                let twoDaysAgo = dateFormatter.string(from: now.addingTimeInterval(-24 * 60 * 60 * 2))
-                let threeDaysAgo = dateFormatter.string(from: now.addingTimeInterval(-24 * 60 * 60 * 3))
-                let fourDaysAgo = dateFormatter.string(from: now.addingTimeInterval(-24 * 60 * 60 * 4))
-                let possibleDates = [today, oneDayAgo, twoDaysAgo, threeDaysAgo, fourDaysAgo]
-                XCTAssertTrue(possibleDates.contains(date))
+        let today = dateFormatter.string(from: now)
+        let oneDayAgo = dateFormatter.string(from: now.addingTimeInterval(-24 * 60 * 60 * 1))
+        let twoDaysAgo = dateFormatter.string(from: now.addingTimeInterval(-24 * 60 * 60 * 2))
+        let threeDaysAgo = dateFormatter.string(from: now.addingTimeInterval(-24 * 60 * 60 * 3))
+        let fourDaysAgo = dateFormatter.string(from: now.addingTimeInterval(-24 * 60 * 60 * 4))
+        let possibleDates = [today, oneDayAgo, twoDaysAgo, threeDaysAgo, fourDaysAgo]
+        XCTAssertTrue(possibleDates.contains(date))
 
-                let datePattern = #"^\d{4}[-]\d{2}[-]\d{2}$"# // 2021-05-07
-                XCTAssertNotNil(date.range(of: datePattern, options: .regularExpression))
+        let datePattern = #"^\d{4}[-]\d{2}[-]\d{2}$"# // 2021-05-07
+        XCTAssertNotNil(date.range(of: datePattern, options: .regularExpression))
 
-                // Currently, the supported currency codes are 32. The number can be updated if the source is changed.
-                // ref: https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html
-                XCTAssertEqual(referenceRates.rates().count, 32)
-                XCTAssertEqual(referenceRates.rates(baseCurrencyCode: "USD").count, 32)
-                XCTAssertEqual(referenceRates.rates(baseCurrencyCode: "JPY").count, 32)
-                XCTAssertEqual(referenceRates.rates(baseCurrencyCode: "XXX").count, 0)
-            case let .failure(error):
-                fatalError(error.localizedDescription)
-            }
-        }
+        // Currently, the supported currency codes are 32. The number can be updated if the source is changed.
+        // ref: https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html
+        XCTAssertEqual(referenceRates.rates().count, 32)
+        XCTAssertEqual(referenceRates.rates(baseCurrencyCode: "USD").count, 32)
+        XCTAssertEqual(referenceRates.rates(baseCurrencyCode: "JPY").count, 32)
+        XCTAssertEqual(referenceRates.rates(baseCurrencyCode: "XXX").count, 0)
     }
 
-    func testReferenceRatesRate() {
+    func testReferenceRatesRate() async throws {
         let converter = CurrencyConverter(data: testXMLData)
-        converter.fetch { result in
-            switch result {
-            case let .success(referenceRates):
-                XCTAssertEqual(referenceRates.rate(amount: 6, fromCurrencyCode: "EUR", toCurrencyCode: "JPY"), 131.76 * 6)
-                XCTAssertEqual(referenceRates.rate(amount: 10, fromCurrencyCode: "USD", toCurrencyCode: "JPY"), 131.76 / 1.2059 * 10)
-                XCTAssertEqual(referenceRates.rate(amount: 100, fromCurrencyCode: "BRL", toCurrencyCode: "NZD"), 1.6730 / 6.3801 * 100)
-                XCTAssertEqual(referenceRates.rate(amount: 2, fromCurrencyCode: "USD", toCurrencyCode: "EUR"), 1 / 1.2059 * 2)
-                XCTAssertEqual(referenceRates.rate(amount: 2, fromCurrencyCode: "USD", toCurrencyCode: "USD"), 2)
-                XCTAssertEqual(referenceRates.rate(amount: 2, fromCurrencyCode: "USD", toCurrencyCode: "XXX"), nil)
-                XCTAssertEqual(referenceRates.rate(amount: 2, fromCurrencyCode: "XXX", toCurrencyCode: "USD"), nil)
-            case let .failure(error):
-                fatalError(error.localizedDescription)
-            }
-        }
+        let referenceRates = try await converter.fetch()
+        XCTAssertEqual(referenceRates.rate(amount: 6, fromCurrencyCode: "EUR", toCurrencyCode: "JPY"), 131.76 * 6)
+        XCTAssertEqual(referenceRates.rate(amount: 10, fromCurrencyCode: "USD", toCurrencyCode: "JPY"), 131.76 / 1.2059 * 10)
+        XCTAssertEqual(referenceRates.rate(amount: 100, fromCurrencyCode: "BRL", toCurrencyCode: "NZD"), 1.6730 / 6.3801 * 100)
+        XCTAssertEqual(referenceRates.rate(amount: 2, fromCurrencyCode: "USD", toCurrencyCode: "EUR"), 1 / 1.2059 * 2)
+        XCTAssertEqual(referenceRates.rate(amount: 2, fromCurrencyCode: "USD", toCurrencyCode: "USD"), 2)
+        XCTAssertEqual(referenceRates.rate(amount: 2, fromCurrencyCode: "USD", toCurrencyCode: "XXX"), nil)
+        XCTAssertEqual(referenceRates.rate(amount: 2, fromCurrencyCode: "XXX", toCurrencyCode: "USD"), nil)
     }
 
-    func testReferenceRatesRates() {
+    func testReferenceRatesRates() async throws {
         let converter = CurrencyConverter(data: testXMLData)
-        converter.fetch { result in
-            switch result {
-            case let .success(referenceRates):
-                XCTAssertEqual(referenceRates.rates(amount: 1, baseCurrencyCode: "EUR").first(where: { $0.currencyCode == "USD" })?.rate, 1.2059)
-                XCTAssertEqual(referenceRates.rates(amount: 1, baseCurrencyCode: "USD").first(where: { $0.currencyCode == "EUR" })?.rate, 1 / 1.2059)
-                XCTAssertEqual(referenceRates.rates(amount: 1, baseCurrencyCode: "JPY").first(where: { $0.currencyCode == "USD" })?.rate, 1.2059 / 131.76)
-                XCTAssertEqual(referenceRates.rates(amount: 1, baseCurrencyCode: "XXX"), [])
-            case let .failure(error):
-                fatalError(error.localizedDescription)
-            }
-        }
+        let referenceRates = try await converter.fetch()
+        XCTAssertEqual(referenceRates.rates(amount: 1, baseCurrencyCode: "EUR").first(where: { $0.currencyCode == "USD" })?.rate, 1.2059)
+        XCTAssertEqual(referenceRates.rates(amount: 1, baseCurrencyCode: "USD").first(where: { $0.currencyCode == "EUR" })?.rate, 1 / 1.2059)
+        XCTAssertEqual(referenceRates.rates(amount: 1, baseCurrencyCode: "JPY").first(where: { $0.currencyCode == "USD" })?.rate, 1.2059 / 131.76)
+        XCTAssertEqual(referenceRates.rates(amount: 1, baseCurrencyCode: "XXX"), [])
     }
 
     private let testXMLData = """
