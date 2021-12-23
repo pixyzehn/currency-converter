d7d6bcb656b8b643af4fb3dc986acf5d360c8638
Hiroki Nagasawa <2482478+pixyzehn@users.noreply.github.com>
2021-12-23 15:20:11 +0900

    Add async version of fetch method

--- Sources/CurrencyConverter/CurrencyConverter.swift
+++ Sources/CurrencyConverter/CurrencyConverter.swift
@@ -23,4 +23,18 @@ public class CurrencyConverter {
         }
         parser.parse()
     }
+
+    /// Asynchronously fetch the latest reference rates from the source.
+    public func fetch() async throws -> ReferenceRates {
+        try await withCheckedThrowingContinuation { continuation in
+            fetch { result in
+                switch result {
+                case .success(let referenceRates):
+                    continuation.resume(returning: referenceRates)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
 }
