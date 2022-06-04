97ac32db1fcb6cebc718118725e63742b5ff6924
Hiroki Nagasawa <2482478+pixyzehn@users.noreply.github.com>
2022-06-04 15:59:41 +0900

    Drop older os versions

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "CurrencyConverter",
-    platforms: [.iOS(.v13), .macOS(.v10_15)],
+    platforms: [.iOS(.v14), .macOS(.v11)],
     products: [
         .library(
             name: "CurrencyConverter",
